f85c416918b09a325b5baa78ae08e0e3ef55c800
Bob <bob@example.com>
2021-04-13 12:15:59 -0400

    -Line 7 : Changed it so the first 3 numbers added are enclosed in parenthesis so they add up first then are divided. -line 16: changed input from n2 to n3 so all variables are correctly set -added comments on the lines detailing the changes listed here

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f; //---added Perenthesis here so the numbers are added up first
 }
 
 int main()
@@ -13,7 +13,7 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3; //---this was n2, changed to n3 so all values are used
 
 	float a = average(n1, n2, n3);
 
